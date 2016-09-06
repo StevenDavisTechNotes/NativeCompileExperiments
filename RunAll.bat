9d7110d1aa70e2a245b22404305adf4f9f650c23
Steven Davis <davis049@att.net>
2016-09-06 13:56:31 -0700

    Got working with Visual Studio 2015

---
+++ RunAll.bat
@@ -0,0 +1,4 @@
+C:\src\NativeCompileExperiments\VisualStudio2015\CSharpConsole\bin\x64\Release\CSharpConsole.exe
+C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPCliConsole2015.exe
+C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPConsole.exe
+C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPConsole2015.exe
