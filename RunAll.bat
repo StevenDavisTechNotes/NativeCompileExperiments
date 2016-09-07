b87c7022f4b7996af3841438d08580e31a8e6e94
Steven Davis <davis049@att.net>
2016-09-07 11:44:49 -0700

    Added .Net Core C# Solution

--- RunAll.bat
+++ RunAll.bat
@@ -2,3 +2,6 @@ C:\src\NativeCompileExperiments\VisualStudio2015\CSharpConsole\bin\x64\Release\C
 C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPCliConsole2015.exe
 rem C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPConsole.exe
 C:\src\NativeCompileExperiments\VisualStudio2015\x64\Release\CPPConsole2015.exe
+pushd C:\src\NativeCompileExperiments\VisualStudio2015\CSharpCore15
+dotnet run
+popd
