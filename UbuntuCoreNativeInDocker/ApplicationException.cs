ddfe0f10390ff915329981eb492e417fe77e0ae4
StevenDavisTechNotes <AComputing@LightLink.com>
2016-10-22 10:09:11 -0400

    Using DoNet Build Container in Ubuntu

---
+++ UbuntuCoreNativeInDocker/ApplicationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpCore15
+{
+    public class ApplicationException: Exception
+    {
+        public ApplicationException(string msg): base(msg) { }
+    }
+}
---
+++ UbuntuCoreNativeInDocker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//namespace CSharpCore15
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//        }
+//    }
+//}
